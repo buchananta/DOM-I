948937b07e6467077db074d614c913897a0d80bd
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-03 16:04:04 -0500

    finished mvp

--- js/index.js
+++ js/index.js
@@ -43,23 +43,64 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 let codeSnippet = document.getElementById('cta-img');
 //codeSnippet.src = 'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80';
-codeSnippet.src = 'img/header-img.png';
+codeSnippet.src = siteContent.cta['img-src'];
 let middleImage = document.querySelector('#middle-img');
-middleImage.src = 'img/mid-page-accent.jpg';
+middleImage.src = siteContent["main-content"]["middle-img-src"];
 let nav = document.querySelector('nav');
 
 //I really want to get these child nodes appended programmatically, but because the data is JSON, it's super hard to iterate through, or grab specific elements programmatically. I've tried like 3 different ways, probably going to end up settling with a closure.
-//I can see a better way to do this now, but this is surprisingly clean. Plus it's kinda neat.
+//I can see a better way to do this now, but this is surprisingly clean. Plus it's kinda neat.o
+
+const nextItemClosure = function(item) {
+  let keys = Object.keys(item);
+  keys.reverse();
+  return () => item[keys.pop()];
+}
 const nextNav= function() {
   let keys = Object.keys(siteContent.nav)
   keys.reverse();
   return () => siteContent.nav[keys.pop()];
 }();
-
-
 for(node of nav.children) {
   node.textContent = nextNav();
+  node.style.color = 'green';
 }
- 
+const first = document.createElement('a');
+first.textContent = 'asdf';
+const second = document.createElement('a');
+second.textContent = 'jkl;';
+nav.prepend(first);
+nav.appendChild(second);
+for(node of nav.children) {
+  node.style.color = 'green';
+}
 
-nav.style.color = 'green'; 
+
+let cta = document.querySelector('.cta');
+cta.querySelector('h1').textContent = siteContent.cta.h1;
+cta.querySelector('button').textContent = siteContent.cta.button;
+//I'm wayyy to lazy at this point to figure out how to iterate over nested crap easily
+//especially when this is so ambigiously labeled I have to look at the original.html just to figure out where it's supposed to go. Just gonna get this done 'fast' rather than well
+let topContent = document.querySelector('.top-content');
+topContent.querySelectorAll('.text-content h4')[0].textContent = siteContent['main-content']['features-h4'];
+topContent.querySelectorAll('.text-content p')[0].textContent = siteContent['main-content']['features-content'];
+topContent.querySelectorAll('.text-content h4')[1].textContent = siteContent['main-content']['about-h4'];
+topContent.querySelectorAll('.text-content p')[1].textContent = siteContent['main-content']['about-content'];
+
+let bottomContent = document.querySelector('.bottom-content');
+bottomContent.querySelectorAll('.text-content h4')[0].textContent = siteContent['main-content']['services-h4'];
+bottomContent.querySelectorAll('.text-content p')[0].textContent = siteContent['main-content']['services-content'];
+bottomContent.querySelectorAll('.text-content h4')[1].textContent = siteContent['main-content']['product-h4'];
+bottomContent.querySelectorAll('.text-content p')[1].textContent = siteContent['main-content']['product-content'];
+bottomContent.querySelectorAll('.text-content h4')[2].textContent = siteContent['main-content']['vision-h4'];
+bottomContent.querySelectorAll('.text-content p')[2].textContent = siteContent['main-content']['vision-content'];
+
+
+
+
+let contact = document.querySelector('.contact');
+const nextContact = nextItemClosure(siteContent.contact);
+for(node of contact.children)
+  node.textContent = nextContact();
+let footer = document.querySelector('footer');
+footer.innerHTML = `<p> ${siteContent.footer.copyright}</p>`;
